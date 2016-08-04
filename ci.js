e2acc10a184b8d83d651913f1351ad285a3c3d9b
jingnanshi <jshi@g.hmc.edu>
2016-08-05 06:26:13 +0800

    create function place holders in js

--- ci.js
+++ ci.js
@@ -12,3 +12,88 @@ function makeyun(){
 		}
 	}
 }
+
+function makeyun(){
+
+}
+
+function lookup(zi){
+
+}
+
+function linetype(sc,i){
+
+}
+
+function classify(){
+
+}
+
+function ispunc(zi){
+
+}
+
+function sortdict(mydict){
+
+}
+
+function guessnext(zi){
+
+}
+
+function guesslast(zi,foo){
+
+}
+
+function guesswithpos(zi,pos,dir){
+
+}
+
+function posinsent(c,ind){
+
+}
+
+function midsent(x,l){
+
+}
+
+function popularity(zi){
+
+}
+
+function isOK(pai,z,i,yg,usedyg){
+
+}
+
+function repeatOK(zi,result,output,pai){
+
+}
+
+function getstruct(pai){
+
+}
+
+function unmark(pai){
+
+}
+
+function mark(pai,ci){
+
+}
+
+function splitbyy(pai){
+
+}
+
+function poswithstruct(struct,ind){
+
+}
+
+// http://stackoverflow.com/questions/894860/set-a-default-parameter-value-for-a-javascript-function
+function write(pai,ys,dir = -1){
+
+}
+
+function getrandy(n,bounds=[20,1000]){
+	
+}
